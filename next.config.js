de08921d162d966d0547e4bceabda9f8c84c56e9
Marco Worms <marcogworms@gmail.com>
2022-07-14 11:43:46 -0300

    add ts build process

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv-webpack')
 
-module.exports = ({
-	plugins: [new Dotenv()],
-	images: {
-		formats: ['image/avif', 'image/webp'],
-		domains: [
-			'rawcdn.githack.com',
-			'images.ctfassets.net'
-		],
-	},
-	env: {
-		REDIS_URL: process.env.REDIS_URL,
-		LIVEBLOCKS_SECRET_KEY: process.env.LIVEBLOCKS_SECRET_KEY,
-		WEBSITE_URI: 'https://yfu.major.tax/',
-	}
-});
+module.exports = {
+  plugins: [new Dotenv()],
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    domains: ['rawcdn.githack.com', 'images.ctfassets.net'],
+  },
+  env: {
+    REDIS_URL: process.env.REDIS_URL,
+    LIVEBLOCKS_SECRET_KEY: process.env.LIVEBLOCKS_SECRET_KEY,
+    WEBSITE_URI: 'https://yfu.major.tax/',
+  },
+  distDir: 'dist',
+}
