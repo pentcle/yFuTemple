86d061d58e887c36bd348174471afdeb6f203a75
Majorfi <major-tom.eth@pm.me>
2023-05-16 16:17:45 +0200

    chore: testing API

--- pages/api/token/[tokenID].tsx
+++ pages/api/token/[tokenID].tsx
@@ -1,4 +1,12 @@
+const JSONContent = {
+	'name': 'YFU',
+	'description': 'Artwork Sourced from yFu, the Comic.',
+	'image': 'https://yfu.vercel.app/artwork.png',
+	'animation_url': 'https://yfu.vercel.app/artwork.mp4',
+	'external_url': 'https://yfu.vercel.app',
+	'attributes': []
+};
+
 export default async function handler(_: any, res: any): Promise<void> {
-	res.writeHead(302, {Location: '/artwork.mp4'});
-	res.end();
+	res.status(200).json(JSONContent);
 }
